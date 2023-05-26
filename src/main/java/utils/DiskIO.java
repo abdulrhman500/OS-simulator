46dd08129844d4288fca75cf731f1d01fde3b34e
Abdulrahman Fahmy <abdulrhmanabotor@gmail.com>
2023-05-26 20:17:37 +0300

    Updated DiskIO and Memory getLowerBound

--- src/main/java/utils/DiskIO.java
+++ src/main/java/utils/DiskIO.java
@@ -2,8 +2,6 @@ package utils;
 
 import java.io.*;
 import java.util.Hashtable;
-
-import Main.Constants;
 import memory.Memory;
 import memory.MemoryWord;
 import process.Process;
@@ -25,61 +23,84 @@ public class DiskIO {
 
     Hashtable<Integer, Process> stringProcessHashtable = new Hashtable<>();
 
-    public void storeToDisk(Process process) throws IOException {
-        String fileName = getProcessFileName(process.getId());
-        File processFile = new File(fileName);
-        if (!processFile.exists()) {
-            processFile.createNewFile();
+    public void storeToDisk(int processID) {
+        FileWriter fileWriter = null;
+        BufferedWriter bufferedWriter = null;
+        try {
+            String fileName = getProcessFileName(processID);
+            File processFile = new File(fileName);
+            if (!processFile.exists())
+                processFile.createNewFile();
+
+            fileWriter = new FileWriter(processFile, true);
+            bufferedWriter = new BufferedWriter(fileWriter);
+            int lowerBound = Memory.getInstance().getLowerBound(processID);
+
+            for (int i = 0; i < PROCESS_SPACE; i++) {
+                MemoryWord curr = Memory.getInstance().loadWord(i + lowerBound);
+                String Name = curr.getName();
+                Object value = curr.getValue();
+                String dataType = getDataType(value);
+
+                bufferedWriter.write(Name);
+                bufferedWriter.newLine();
+                bufferedWriter.write(value + "");
+                bufferedWriter.newLine();
+                bufferedWriter.write(dataType);
+                bufferedWriter.newLine();
+
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (fileWriter == null || bufferedWriter == null) return;
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                fileWriter.close();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
-        FileOutputStream fileWriter = new FileOutputStream(processFile);
-        ObjectOutputStream Writer = new ObjectOutputStream(fileWriter);
-        int lowerBound = process.getLowerBound();
-        for (int i = 0; i < Constants.PROCESS_SPACE; i++) {
-            MemoryWord curr = Memory.getInstance().loadWord(lowerBound + i);
-            Writer.writeObject(curr);
-        }
-        Writer.flush();
-        Writer.close();
-        fileWriter.close();
-        process.setLowerBound(-1);
-        stringProcessHashtable.put(process.getId(), process);
     }
 
-    public Process loadFromDisk(int processID) {
+    private String getDataType(Object value) {
+        if (value instanceof Integer)
+            return Integer.class.getName();
+        if (value instanceof String)
+            return String.class.getName();
+        if (value instanceof Double)
+            return Double.class.getName();
+        if (value instanceof Float)
+            return Float.class.getName();
+
+        return Object.class.getName();
+
+    }
+
+    public void loadFromDisk(int processID, int address) {
 
         String fileName = getProcessFileName(processID);
 
         File processFile = new File(fileName);
 
         try {
-            FileInputStream fileInputStream = new FileInputStream(processFile);
-            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
-            MemoryWord[] info = new MemoryWord[PROCESS_SPACE];
+            FileReader fileReader = new FileReader(fileName);
+
+            // Create a BufferedReader object to read text
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+
             for (int i = 0; i < PROCESS_SPACE; i++) {
-                MemoryWord curr = (MemoryWord) objectInputStream.readObject();
-                info[i] = curr;
+                String Name = bufferedReader.readLine();
+                Object value = bufferedReader.readLine();
+                String dataType = bufferedReader.readLine();
+                Memory.getInstance().storeWord(address + i, Name, changeObjectType(value, dataType));
             }
-            int lowerBound = (Integer) info[3].getValue();
-
-            if (lowerBound == -1) {
-                int prID = (Integer) info[0].getValue();
-                lowerBound = Memory.getInstance().getLowerBound(prID - 1);
-                info[3].setValue(lowerBound);
-            }
-
-            for (int i = 0; i < info.length; i++) {
-                Memory.getInstance().storeWord(lowerBound + i, info[i].getName(), info[i].getValue());
-            }
-
-            stringProcessHashtable.get(processID).setLowerBound(lowerBound);
-            Process tmp = stringProcessHashtable.get(processID);
-            stringProcessHashtable.remove(processID);
-            return tmp;
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         } catch (IOException e) {
-            throw new RuntimeException(e);
-        } catch (ClassNotFoundException e) {
             throw new RuntimeException(e);
         } finally {
             if (processFile.exists()) {
@@ -87,6 +108,21 @@ public class DiskIO {
             }
         }
     }
+
+    private Object changeObjectType(Object value, String dataType) {
+
+        if (dataType.equals(String.class.getName()))
+            return (String) value;
+        if (dataType.equals(Integer.class.getName()))
+            return (Integer) value;
+        if (dataType.equals(Double.class.getName()))
+            return (Double) value;
+        if (dataType.equals(Float.class.getName()))
+            return (Float) value;
+
+        return (Object) value;
+    }
+
     private int getLowerBoundOf(int processID) {
 
         return 0;
